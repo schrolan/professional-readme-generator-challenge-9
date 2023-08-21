73586e8783ace3795cfaa6caf1dfde86f226ae55
schrolan <schrolan000@gmail.com>
2023-08-21 17:35:13 -0500

    Finished the project

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,77 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ {
+    type: "input",
+    name: "title",
+    message: "Please choose a title for your project:",
+    default: "My README."
+},
+{
+    type: "input",
+    name: "description",
+    mesage: "Please write a description of the project:",
+    default: "Your description goes here."
+},
+{
+    type: "input",
+    name: "installation",
+    message: "Please put the steps for installation here:",
+    default: "Installation steps goes here."
+},
+{
+    type: "input",
+    name: "usage_information",
+    message: "Please write your usage information here:",
+    default: "Usage information goes here."
+},
+{
+    type: "list",
+    name: "license",
+    message: "Which license fits your project?",
+    default: "agpl",
+    choices: ['agpl', 'apache', 'mit', 'no license']
+},
+{
+    type: "input",
+    name: "contribution",
+    message: "Please write how others can contribute here:",
+    default: "Contribution information goes here."
+},
+{
+    type: "input",
+    name: "tests",
+    message: "Please write how to run tests for this project here:",
+    default: "Test information goes here."
+},
+{
+    type: "input",
+    name: "username",
+    message: "Please write your github username here:",
+    default: "Username information goes here."
+},
+{
+    type: "input",
+    name: "email",
+    message: "Please write the email associated with your github here:",
+    default: "Github associated email goes here."
+},
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), error => {
+        if (error) throw error
+  
+        console.log('HTML saved!')
+      })}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {  inquirer.prompt(questions).then((data) => {
+    console.log(JSON.stringify(data, null, " "));
+    writeToFile("./example/README.md", data);
+});}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,80 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'no license') {
+    console.log(license)
+    return `
+  ![badge](https://img.shields.io/badge/license-${license}-blue)
+    `;
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'no license') {
+    return `
+    [${license}](https://choosealicense.com/licenses/${license})
+      `;
+    } else {
+      return ' ';
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'no license') {
+    return `
+    ## [License](#table-of-contents)
+    The application is covered under the following license:
+    ${renderLicenseLink(license)}
+      `;
+    } else {
+      return ' ';
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  console.log(data)
 
+  return `  # ${data.title}
+
+
+  ## Licensing:
+  ${renderLicenseBadge(data.license)}
+  
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage_information)
+  - [License](#license)
+  - [Contribution](#contribution)
+  - [Testing](#tests)
+  - [Username](#username)
+  - [Email](#email)
+
+
+  # ${data.description}
+  
+  # ${data.installation}
+  
+  # ${data.usage_information}
+  
+  # ${data.license}
+  
+  # ${data.contribution}
+  
+  # ${data.tests}
+  
+  # ${data.username}
+  
+  # ${data.email}
+  
+  ${renderLicenseSection(data.license)}
 `;
 }
 
